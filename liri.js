1f32a98c560c5bc8b0d36087fc02a69dd29658bd
James Bajamundi <jamesbjmnd@gmail.com>
2019-11-29 22:34:10 -0800

    fixed function to work

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 //import keys.js file and store it in a variable
 var keys = require("./keys.js");
+var request = require('request');
 
 let userInput = process.argv[2];
 let userQuery = process.argv.slice(3).join(".");
@@ -19,14 +20,17 @@ command(userInput, userQuery);
 
 function movieThis() {
     console.log(`Searching for ${userQuery}...`);
-    request('http://www.omdbapi.com?t=' + userQuery + '$apikey=trilogy', function (error, response) {
+    request('http://www.omdbapi.com/?t=' + userQuery + '&apikey=d9209737', function (error, _response, body) {
         let userMovie = JSON.parse(body);
 
-        let ratingsArr = userMovie.Ratings;
-        if (ratingsArr.length > 2) {
-        }
-        if (!error && response.statusCode === 200) {
-            console.log(`Title: ${userMovie.Title} \nCast: ${userMovie.Actors} \nRelease: ${userMovie.Year} \nIMDb Rating: ${userMovie.imdbRating} \nRotten Tomatoes Rating: ${userMovie.Ratings[1].Value} \nPlot : ${userMovie.Plot} \n`)
+        if (!error) {
+            console.log("\nTitle: " + userMovie.Title);
+            console.log("Release Year: " + userMovie.Year);
+            console.log("IMDB Rating: " + userMovie.imdbRating);
+            console.log("Country: " + userMovie.Country);
+            console.log("Language: " + userMovie.Language);
+            console.log("Plot: " + userMovie.Plot);
+            console.log("Actors: " + userMovie.Actors + '\n');
         }
     })
 }
